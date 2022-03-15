aebfd690b4b1c6a9b924bcb3838847d44c0acd02
Manu <manugeo4@gmail.com>
2022-03-15 12:43:31 -0400

    data file is created

---
+++ data.go
@@ -0,0 +1,10 @@
+package main
+
+var ages = map[string]int{
+	"shiva":  39,
+	"julie":  12,
+	"juliet": 45}
+var ages1 = map[string]int{
+	"shiva":  39,
+	"julie":  12,
+	"juliet": 44}
--- main.go
+++ main.go
@@ -5,14 +5,7 @@ import (
 )
 
 func main() {
-	ages := map[string]int{
-		"shiva":  39,
-		"julie":  12,
-		"juliet": 45}
-	ages1 := map[string]int{
-		"shiva":  39,
-		"julie":  12,
-		"juliet": 44}
+
 	mapSort(ages)
 	fmt.Println(mapEqual(ages, ages1))
 
