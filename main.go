5a56b5c6a8056a3df70a66525817a419010ca922
Manu <manugeo4@gmail.com>
2022-03-15 10:47:15 -0400

    learning map

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
 package main
 
-fmt.Println("Hello World")
+import (
+	"fmt"
+)
+
+func main() {
+	ages := make(map[string]int)
+
+	//below is a map literal
+	ages1 := map[string]int{
+		"julie":  12,
+		"juliet": 45}
+
+	fmt.Println(ages)
+	fmt.Println(ages1)
+	fmt.Println(ages1["juliet"]) //map elements can be accessed as a subscript notation
+	delete(ages1, "juliet")
+	fmt.Println(ages1)
+	ages["Babs"] += 1
+	fmt.Println(ages["Babs"])
+
+}
